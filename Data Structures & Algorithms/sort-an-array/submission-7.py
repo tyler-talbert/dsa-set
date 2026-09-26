79be9583223771433572dd0165d88ca73fad24d2
Tyler Talbert <tylertalbert02@gmail.com>
2026-09-26 12:07:10 -0700

    Add: sort-an-array - submission-7

---
+++ Data Structures & Algorithms/sort-an-array/submission-7.py
@@ -0,0 +1,8 @@
+import heapq
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        heapq.heapify(nums)
+
+        return [heapq.heappop(nums) for _ in range(len(nums))]
